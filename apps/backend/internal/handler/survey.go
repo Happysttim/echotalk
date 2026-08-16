3d75098e6f0d4d4d0a219746a0ee261022b95a21
Happysttim <gmlsdyd1023@naver.com>
2026-08-17 02:08:46 +0900

    updated commit

--- apps/backend/internal/handler/survey.go
+++ apps/backend/internal/handler/survey.go
@@ -3,9 +3,12 @@ package handler
 import (
 	"echotalk/internal/model"
 	"echotalk/internal/service"
+	"encoding/base64"
+	"encoding/json"
 	"net/http"
 	"slices"
 	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/v2/bson"
@@ -24,6 +27,12 @@ const (
 )
 
 var Limits = []int{LimitSmall, LimitMedium, LimitLarge}
+var Types = []string{TypeID, TypeUpdatedAt}
+
+type SurveyCursor struct {
+	SurveyID  string `json:"survey_id"`
+	UpdatedAt string `json:"updated_at"`
+}
 
 type SurveyHandler struct {
 	surveyService *service.SurveyService
@@ -81,10 +90,80 @@ func (handler *SurveyHandler) GetSurvey(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"status": "ok", "survey": survey})
 }
 
-// @Router /surveys?skip={string}&limit={number} [get]
+// @Router /surveys?type={string}&cursor={string}&limit={number} [get]
 func (handler *SurveyHandler) GetSurveyPage(c *gin.Context) {
-	skip := c.Query("skip")
-	limit := c.Query("number")
+	findType := c.Query("type")
+	cursorBase64 := c.Query("cursor")
+	limit := c.Query("limit")
+
+	filter := bson.M{}
+	sort := bson.D{}
+
+	if !slices.Contains(Types, findType) {
+		findType = TypeID
+	}
+
+	if findType == TypeUpdatedAt {
+		sort = bson.D{
+			{Key: "_id", Value: -1},
+			{Key: "updated_at", Value: -1},
+		}
+	} else {
+		sort = bson.D{
+			{Key: "_id", Value: -1},
+		}
+	}
+
+	if cursorBase64 != "" {
+		decodeBytes, err := base64.RawURLEncoding.DecodeString(cursorBase64)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "bad request"})
+			return
+		}
+
+		var cursor SurveyCursor
+		if err := json.Unmarshal(decodeBytes, &cursor); err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "bad request"})
+			return
+		}
+
+		if findType == TypeUpdatedAt {
+			surveyID, err := bson.ObjectIDFromHex(cursor.SurveyID)
+			if err != nil {
+				c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "bad request"})
+				return
+			}
+
+			updatedAt, err := time.Parse(time.RFC3339Nano, cursor.UpdatedAt)
+			if err != nil {
+				c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "bad request"})
+				return
+			}
+
+			filter["$or"] = bson.A{
+				bson.M{
+					"updated_at": updatedAt,
+					"_id": bson.M{
+						"$lt": surveyID,
+					},
+				},
+				bson.M{
+					"updated_at": bson.M{
+						"$lt": updatedAt,
+					},
+				},
+			}
+		} else {
+			surveyID, err := bson.ObjectIDFromHex(cursor.SurveyID)
+			if err == nil {
+				c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "bad request"})
+				return
+			}
+			filter["_id"] = bson.M{
+				"$lt": surveyID,
+			}
+		}
+	}
 
 	limitNumber, err := strconv.Atoi(limit)
 
@@ -97,18 +176,40 @@ func (handler *SurveyHandler) GetSurveyPage(c *gin.Context) {
 	}
 
 	ctx := c.Request.Context()
-	filter := bson.M{
-		"_id": bson.M{
-			"$lt": skip,
-		},
-	}
 	surveys, err := handler.surveyService.GetFilteredSurveys(
 		ctx,
 		filter,
-		options.Find().SetSort(bson.D{
-			{Key: "_id", Value: -1},
-		}).SetLimit(int64(limitNumber)),
+		options.Find().SetSort(sort).SetLimit(int64(limitNumber)),
 	)
 
-	c.JSON(http.StatusOK, gin.H{"status": "ok", "skip": surveys[len(surveys)-1].ID.Hex(), "surveys": surveys})
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "internal server error"})
+		return
+	}
+
+	var nextSkip string
+	var nextID string
+	var nextAt string
+
+	if len(surveys) > 0 {
+		nextID = surveys[len(surveys)-1].ID.Hex()
+		nextAt = surveys[len(surveys)-1].UpdatedAt.Format(time.RFC3339Nano)
+	}
+
+	if nextID != "" && nextAt != "" {
+		nextCursor := SurveyCursor{
+			SurveyID:  nextID,
+			UpdatedAt: nextAt,
+		}
+
+		jsonBytes, err := json.Marshal(nextCursor)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "internal server error"})
+			return
+		}
+
+		nextSkip = base64.RawURLEncoding.EncodeToString(jsonBytes)
+	}
+
+	c.JSON(http.StatusOK, gin.H{"status": "ok", "skip": nextSkip, "surveys": surveys})
 }
